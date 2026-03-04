c2d7fdf417581d69763efa4e8436c272fce6788f
andreapelizza <80447522+andreapelizza@users.noreply.github.com>
2026-03-04 14:49:43 +0100

    1

--- cad-vvf/database/seed.sql
+++ cad-vvf/database/seed.sql
@@ -4,7 +4,7 @@ INSERT INTO protocols (name, description) VALUES ('Emergenza VVF Demo', 'Protoco
 
 INSERT INTO questions (protocol_id, text, next_if_yes, next_if_no) VALUES
 (1, 'Ci sono persone in pericolo?', 2, 3),
-(1, 'Chiedere se ci sono feriti gravi', NULL, NULL),
+(1, 'Ci sono feriti gravi?', NULL, NULL),
 (1, 'Ci sono fiamme visibili?', 4, 5),
-(1, 'Chiedere se l\'incendio è contenuto', NULL, NULL),
+(1, 'L\'incendio è contenuto?', NULL, NULL),
 (1, 'Fine protocollo', NULL, NULL);
